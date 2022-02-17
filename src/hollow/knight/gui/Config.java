01cc2f7450978f7d22fe3b643a315ca0baf22127
dplochcoder <daniel.ploch.coder@gmail.com>
2022-02-16 23:27:22 -0600

    Move config handling to AppData, with platform-independent resolution.

--- src/hollow/knight/gui/Config.java
+++ src/hollow/knight/gui/Config.java
@@ -7,28 +7,29 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import net.harawata.appdirs.AppDirsFactory;
 
 public final class Config {
   private final Path path;
   private final Map<String, String> props;
-  
+
   private Config(Path path, Map<String, String> props) {
     this.path = path;
     this.props = new HashMap<>(props);
   }
-  
+
   public String get(String prop) {
     return get(prop, "");
   }
-  
+
   public String get(String prop, String defaultValue) {
     return props.getOrDefault(prop, defaultValue);
   }
-  
+
   public boolean isEmpty() {
     return props.isEmpty();
   }
-  
+
   public void set(String prop, String value) {
     if (value.isEmpty()) {
       props.remove(prop);
@@ -36,7 +37,7 @@ public final class Config {
       props.put(prop, value);
     }
   }
-  
+
   public void save() throws IOException {
     StringBuilder content = new StringBuilder();
     props.keySet().stream().sorted().forEachOrdered(k -> {
@@ -45,28 +46,31 @@ public final class Config {
       content.append(props.get(k));
       content.append('\n');
     });
-    
+
+    Files.createDirectories(path.getParent());
     Files.write(path, content.toString().getBytes(StandardCharsets.UTF_8));
   }
-  
+
   public static Config load() {
-    Path abs = Paths.get(System.getProperty("user.dir"), "HKSpoilerViewer.cfg").toAbsolutePath();
+    Path abs = Paths
+        .get(AppDirsFactory.getInstance().getUserDataDir("HKSpoilerViewer", null, "dplochcoder"));
     return load(abs);
   }
-  
+
   public static Config load(Path path) {
     Map<String, String> props = new HashMap<>();
     try {
       for (String line : Files.readAllLines(path)) {
         String[] parts = line.split("=", 2);
-        if (parts.length != 2) continue;
-        
+        if (parts.length != 2)
+          continue;
+
         props.put(parts[0], parts[1]);
       }
     } catch (IOException ex) {
       props.clear();
     }
-    
+
     return new Config(path, props);
   }
 }
