4a15be3c34e91fe093b45a244c6a6c9b536084d6
dplochcoder <daniel.ploch.coder@gmail.com>
2022-06-01 02:07:47 -0400

    Fix warning.

--- src/hollow/knight/logic/ICDLException.java
+++ src/hollow/knight/logic/ICDLException.java
@@ -2,6 +2,8 @@ package hollow.knight.logic;
 
 // An exception while saving to ICDL format.
 public final class ICDLException extends Exception {
+  private static final long serialVersionUID = 1L;
+
   public ICDLException(String message) {
     super(message);
   }
