b7230cd7536a152d59edd49c933e2514fd3fefde
dplochcoder <daniel.ploch.coder@gmail.com>
2022-08-08 22:22:25 -0400

    Make the version identifier have 4 pieces.

--- SpoilerViewer/Application/hollow/knight/logic/Version.java
+++ SpoilerViewer/Application/hollow/knight/logic/Version.java
@@ -8,24 +8,33 @@ public abstract class Version {
 
   public abstract int minor();
 
+  public abstract int build();
+
   public abstract int patch();
 
   public static Version parse(String txt) throws ParseException {
     String[] args = txt.split("\\.");
-    int major = Integer.parseInt(args[0]);
-    int minor = Integer.parseInt(args[1]);
-    int patch = 0;
-    if (args.length == 3) {
-      patch = Integer.parseInt(args[2]);
-    } else if (args.length != 2) {
+    if (args.length < 2 || args.length > 4) {
       throw new ParseException("Invalid version: " + txt);
     }
 
-    return new AutoValue_Version(major, minor, patch);
+    int major = Integer.parseInt(args[0]);
+    int minor = Integer.parseInt(args[1]);
+    int build = 0;
+    int patch = 0;
+    if (args.length >= 3) {
+      build = Integer.parseInt(args[2]);
+      if (args.length >= 4) {
+        patch = Integer.parseInt(args[3]);
+      }
+    }
+
+    return new AutoValue_Version(major, minor, build, patch);
   }
 
   @Override
   public String toString() {
-    return major() + "." + minor() + (patch() != 0 ? "." + patch() : "");
+    return major() + "." + minor() + (build() + patch() != 0 ? "." + build() : "")
+        + (patch() != 0 ? "." + patch() : "");
   }
 }
